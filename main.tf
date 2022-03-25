7d0bb58692d60818c4d16a15be29104d31307ef3
arnhaa <arnehaagensen@gmail.com>
2022-03-25 14:19:46 +0100

    updated with env variable

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket_object" "delegated_service_documentation" {
   bucket = "061938725231-infrademo-delegated-service-documentation"
-  key    = "stage/${var.current_account_id}/${var.name_prefix}.json"
+  key    = "${var.env}/${var.current_account_id}/${var.name_prefix}.json"
   acl    = "bucket-owner-full-control"
   # local.service_documentation_bucket
   content = jsonencode({
