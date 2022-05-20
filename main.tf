154de54bbc59486236e76cf6ead392e74d21889e
Arne Haagensen <arnehaagensen@gmail.com>
2022-05-20 11:38:54 +0200

    Update main.tf
    removed reference to aws provider version

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
 terraform {
   required_version = ">= 1.0.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.74.0, < 4.0.0"
-    }
-  }
 }
 
 resource "aws_s3_bucket_object" "delegated_service_documentation_prod" {
