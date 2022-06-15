3ba34aee95002cd4612ea3b84393fed37dfa5b1b
arnhaa <arnehaagensen@gmail.com>
2022-06-15 10:26:30 +0200

    split into two sub modules for support of solutions without api gateway

---
+++ modules/plain-servicedoc/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_version = ">= 1.0.0"
+}
+
+resource "aws_s3_bucket_object" "delegated_service_documentation_prod" {
+  count = "${var.env == "prod" ? 1 : 0}"
+  bucket = "727646359971-common-services-delegated-service-documentation"
+  key    = "${var.env}/${var.current_account_id}/${var.application_name}.json"
+  acl    = "bucket-owner-full-control"
+
+  content_type = "application/json"
+  content      = jsonencode({
+    applicationname = var.application_name
+    slack           = var.slack
+    about_file     = "toberemoved"
+    owner          = var.owner
+    technicalowner = var.technicalowner
+    servicesla     = var.servicesla
+    growthmetric   = var.growthmetric
+    aktivitetskode = var.aktivitetskode
+
+  })
+}
+
+resource "aws_s3_bucket_object" "delegated_service_documentation_test" {
+  count = "${var.env != "prod" ? 1 : 0}"
+  bucket = "167605120605-common-services-delegated-service-documentation"
+  key    = "${var.env}/${var.current_account_id}/${var.application_name}.json"
+  acl    = "bucket-owner-full-control"
+
+  content_type = "application/json"
+  content      = jsonencode({
+    applicationname = var.application_name
+    slack           = var.slack
+    about_file     = "toberemoved"
+    owner          = var.owner
+    technicalowner = var.technicalowner
+    servicesla     = var.servicesla
+    growthmetric   = var.growthmetric
+    aktivitetskode = var.aktivitetskode
+
+  })
+}
+
---
+++ modules/plain-servicedoc/variables.tf
@@ -0,0 +1,45 @@
+variable "application_name" {
+  description = "The name of this application"
+  type        = string
+}
+
+variable "env" {
+  description = "Variable to define if the setup is for dev, test, stage, prod env"
+  type        = string
+}
+
+variable "current_account_id" {
+  description = "Account number of current account"
+  type        = string
+}
+
+variable "slack" {
+  description = "Name of slack channel to reach team responsible for the application"
+  type        = string
+}
+
+variable "owner" {
+  description = "Email of the financial owner of the solution"
+  type        = string
+}
+
+variable "technicalowner" {
+  description = "Email of the technical owner of the solution "
+  type        = string
+}
+
+variable "servicesla" {
+  description = "SLA set for service listed as procentage of time guaranteed available"
+  type        = string
+}
+
+variable "growthmetric" {
+  description = "A prefix used for naming resources."
+  type        = string
+}
+
+variable "aktivitetskode" {
+  description = "A prefix used for naming resources."
+  type        = string
+}
+
